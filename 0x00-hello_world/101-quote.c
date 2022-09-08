907284b6fc806e88302e285b49035e5580366dc6
mufasatech <princedapo2000@gmail.com>
2022-09-08 21:39:35 +0000

    pritn without any function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - print the string without the use of any function
+* Description: using the main function
+* this program prints "and that piece of art is useful"-Dora Korpar,2015-10-19"
+*Return: 1
+*/
+int main(void)
+{
+	("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n");
+	return (1);
+}
