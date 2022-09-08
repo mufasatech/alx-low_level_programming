913475aeb5209ad004bf066c6b078df5d8084e3b
mufasatech <princedapo2000@gmail.com>
2022-09-08 16:14:26 -0700

    wirite function printing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,15 @@
 #include <unistd.h>
 
 /**
-* main - Prints out the last part of a quote in the standard error.
-* Return: 1
-*/
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
+ */
+
 int main(void)
+
 {
-	write("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n");
+	write("/"and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n");
+
 	return (1);
+
 }
