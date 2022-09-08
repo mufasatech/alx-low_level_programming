c3e03198cc931e56d52d9a58590ecb8edf3bae05
mufasatech <princedapo2000@gmail.com>
2022-09-08 20:28:51 +0000

    printf printing option

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+* main - print the string in the printf fnction
+*
+* Description: using the main function
+* this program prints "with proper grammar, but the outcome is a piece of art,"
+*Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
