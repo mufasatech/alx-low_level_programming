02c9eb2b3cbadcb840fa20e359cf5b4c57e58dd4
mufasatech <princedapo2000@gmail.com>
2022-09-08 20:51:16 +0000

    printf printing option

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - print the string in the printf function
  * Description: using the main function
- * this program prints "with proper grammar, but the outcome is a piece of art,"
+ * this program prints "with proper grammar, but the outcome is a piece of art"
  * Return: 0
  */
 int main(void)
