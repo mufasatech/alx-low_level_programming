e2b3af8c97f23f6395a7ff1417c07b90ed49d9f6
mufasatech <princedapo2000@gmail.com>
2022-09-08 21:44:07 +0000

    print using the write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - print the string without the use of any function
+* main - print the string using the write function
 * Description: using the main function
 * this program prints "and that piece of art is useful"-Dora Korpar,2015-10-19"
 *Return: 1
 */
 int main(void)
 {
-	("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n");
+	write("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n");
 	return (1);
 }
