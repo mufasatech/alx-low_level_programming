2b2b8958df164bc33deb334fddd5ea62c47520ae
mufasatech <princedapo2000@gmail.com>
2022-09-08 21:58:19 +0000

    print using the write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
-* main - print the string in the write function
+* main - print the string in the write function followed by a new line to the standard error
 * Description: using the main function
 * this program prints "and that piece of art is useful"-Dora Korpar,2015-10-19"
 *Return: 1
