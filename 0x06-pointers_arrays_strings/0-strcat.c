4b909093428d9e9c9c70bc538f4913225913f76f
mufasatech <princedapo2000@gmail.com>
2022-09-22 08:54:35 +0000

    concatenating two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,6 +16,6 @@ Char *_strcat(char *dest, char *src)
 	while (dest[index++])
 	dest_len++;
 	for (index = 0; src[index]; index++)
-		dest{dest_len++ } = src[index];
+		dest[dest_len++] = src[index];
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,7 +13,7 @@
 	char *strcat(char *dest, char *src);
 	char *_strncat(char *dest, char *src, int n);
 	char *_strncpy(char *dest, char *src, int n);
-	char *string_toupper(car *);
+	char *string_toupper(char *);
 	char *cap_string(char *);
 	char *leet(char *);
 	char *rot13(char *);
