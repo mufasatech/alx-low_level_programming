1f22b5e9cf2b692e217c43ec44981fc06dd0b6c4
mufasatech <princedapo2000@gmail.com>
2022-09-27 12:59:34 +0000

    constant byte to memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,9 +10,9 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-	unsigned char *s = s,
-	for (i = 0; i < n; i++)
+	int i = 0;
+
+	for (; n > 0; i++)
 	{
 		s[i] = b;
 		n--;
