45245a6aef7d21bf1a0a71e8372c0c9214ce5ee2
mufasatech <princedapo2000@gmail.com>
2022-09-28 09:50:42 +0000

    diagsum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,7 +17,7 @@ void print_diagsums(int *a, int size);
 
 	for (y = 0; y < size; y++)
 	{
-		sun1 = sum2 + a[y * size + y];
+		sum1 = sum2 + a[y * size + y];
 	}
 	for (y = size - 1; y >= 0; y--)
 	{
