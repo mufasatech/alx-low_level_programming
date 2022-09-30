8c645b506e7c3777fb50e8bfe33e6a44c8bf0d51
mufasatech <princedapo2000@gmail.com>
2022-09-30 20:56:22 +0000

    palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -20,5 +20,8 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
 
 #endif
