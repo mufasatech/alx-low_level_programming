b04e688804edd7f193d55d5cd5ce991990c6056a
mufasatech <princedapo2000@gmail.com>
2022-09-30 20:54:42 +0000

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,54 +6,25 @@
  * Return: int/ length of string
  */
 
-int _strlen_recursion(char *s)
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
+/**
+ * find_strlen - Returns the length of a string
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+
+int find_strlen(char *s)
 {
 	int len = 0;
 
-	if (*s == '\0')
-	{
-		return (0);
-	}
-	if (*s != '\0')
+	if (*(s + len))
 	{
 		len++;
-		len += _strlen_recursion(++s);
-		return (len);
+		len += find_strlen(s + len);
 	}
-	return (0);
-}
-
-/**
- * _compareends - checks for matches at either end of a str
- * @s: the string
- * @begin: the start
- * @end: the end
- * Return: 0 or 1
- */
-
-int _compareends(char *s, int begin, int end)
-{
-	if (begin >= end)
-		return (1);
-	if (s[begin] == s[end])
-		return (_compareends(s, (begin + 1), (end - 1)));
-	if (s[begin] == s[end] && begin == end)
-		return (1);
-	return (0);
-}
-/**
- * is_palindrome - checking for palindrome
- * @s: the string
- * Return: 1 if true, 0 if not
- */
-
-int is_palindrome(char *s)
-{
-	int len;
-
-	len = _strlen_resursion(s);
-	if (len == 0 || len == 1)
-		return (1);
-	else
-		return (_compareends(s, 0, len - 1));
+	return (len);
 }
