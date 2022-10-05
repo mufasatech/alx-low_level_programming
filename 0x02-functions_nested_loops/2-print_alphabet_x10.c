0166c8463d936c58a02b94522709b2753320b6d5
mufasatech <princedapo2000@gmail.com>
2022-10-05 12:43:55 +0000

    hotfix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 -prints the alphabet 10x, in lowercase, 
+ * print_alphabet_x10 -prints the alphabet 10x, in lowercase,
  * followed by a new line
  */
 
