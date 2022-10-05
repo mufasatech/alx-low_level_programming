6ad11ba9c28a468344390fb97dd749ca90355d42
mufasatech <princedapo2000@gmail.com>
2022-10-05 15:29:29 +0000

    jack buaer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,7 +19,7 @@ void jack_bauer(void)
 			}
 			else if (i >= 10)
 			{
-				_putchar ((i / 10) + '0');
+				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
 			}
 			if (j < 10)
@@ -28,12 +28,13 @@ void jack_bauer(void)
 					_putchar('0');
 					_putchar(j + '0');
 			}
-			else if (j > 10)
+			else if (j >= 10)
 			{
 				_putchar(':');
 				_putchar(j / 10) + '0');
 				_putchar(j % 10) + '0');
 			}
+			_putchar('\n');
 		}
 	}
 }
