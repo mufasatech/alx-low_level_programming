9d37933c5e12faca14ff8b93f707b4f940dc39ab
mufasatech <princedapo2000@gmail.com>
2022-10-05 16:58:32 +0000

    numbers print from 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,12 @@
 /**
  * print_numbers - Prints the numbers from 0-9.
  */
+
 void print_numbers(void)
 {
-		int num;
+	int x;
 
-		num = 0;
-		num <= 9;
-		num++
-		_putchar((num % 10) + '0');
-
-				_putchar('\n');
+	for (x = 0; x < 10; x++)
+		_putchar(x  + '0');
+	_putchar('\n');
 }
