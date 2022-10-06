fd9e4fbbe0796e5604a85d0a2dc41bee22334d74
mufasatech <princedapo2000@gmail.com>
2022-10-06 12:23:59 +0000

    hotfix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,9 @@ void more_numbers(void)
 
 		for (x = 0; x < 10; x++)
 	{
-			for (y = 0; y <= 14; y++)
+			for (y = 0; y < 15; y++)
 		{
-			_putchar(y);
+			_putchar(y + '0');
 		}
 				_putchar('\n');
 	}
