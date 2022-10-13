1cc6e59dfdd3c6e5480e849c88229eb98d4f2d00
mufasatech <princedapo2000@gmail.com>
2022-10-13 11:10:17 +0000

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,17 +3,18 @@
 /**
  * more_numbers - Prints the numbers 0-14 ten times.
  */
-
 void more_numbers(void)
 {
-	int x, y;
+	int num, count;
 
-		for (x = 0; x < 10; x++)
+	for (count = 0; count <= 9; count++)
 	{
-			for (y = 0; y < 15; y++)
+		for (num = 0; num <= 14; num++)
 		{
-			_putchar(y + '0');
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
 		}
-				_putchar('\n');
+		_putchar('\n');
 	}
 }
